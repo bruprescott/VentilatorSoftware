7defd4249e595f27c2c41339fb6dc92dfc2eccc3
miceuz <mic@hardcore.lt>
2020-04-16 21:35:56 +0300

    #85 illustration commit

--- controller/test/controller_data_packet_ser/controller_data_packet_ser_test.cpp
+++ controller/test/controller_data_packet_ser/controller_data_packet_ser_test.cpp
@@ -2,11 +2,25 @@
 
 #include <stdio.h>
 #include <pb_encode.h>
+#include <pb_decode.h>
 #include <pb_common.h>
 #include "serdes.h"
+#include "network_protocol.pb.h"
 
 #define PACKET_LEN_MAX (32)
 uint8_t tx_buffer[PACKET_LEN_MAX];
+
+struct mydata_t {
+  size_t len;
+  uint8_t buf[16];
+};
+
+static bool bytes_callback(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+    // int *t = (int*) *arg;
+    // printf("!!!decode %d\n", (*t));
+    printf("!!!decode\n");
+    return true;//pb_decode_string(stream, data->buf, data->len);
+}
 
 
 void test_Serialization() {
@@ -31,14 +45,28 @@ void test_Serialization() {
     readingsData[14] = 15;
     readingsData[15] = 16;
 
-    size_t tx_data_length;
-    bool status = serdes_encode_data_packet(readingsData, 16, tx_buffer, PACKET_LEN_MAX, &tx_data_length);
+    size_t encoded_data_length;
+    bool status = serdes_encode_data_packet(readingsData, 16, tx_buffer, PACKET_LEN_MAX, &encoded_data_length);
     for(int i = 0; i < PACKET_LEN_MAX; i++) {
     	printf("%d - %d\n", i, tx_buffer[i]);
     }
-    printf("packet length: %d\n", tx_data_length);
 
-	TEST_ASSERT_EQUAL_INT16(status, 1);
+    printf("packet length: %d\n", (unsigned int) encoded_data_length);
+
+	TEST_ASSERT_EQUAL_INT16(true, status);
+	TEST_ASSERT_EQUAL_INT16(24, encoded_data_length);
+
+    pb_istream_t stream = pb_istream_from_buffer(tx_buffer, PACKET_LEN_MAX);
+    
+    Packet packet = Packet_init_zero;
+    packet.payload.data.data.funcs.decode = bytes_callback;
+
+    status = pb_decode(&stream, Packet_fields, &packet);
+
+	TEST_ASSERT_EQUAL_INT16(true, status);
+	TEST_ASSERT_EQUAL_INT16(3, packet.which_payload);
+    TEST_ASSERT_EQUAL_INT16(ControllerMsgType_DATA, packet.payload.data.msg_type);
+
 }
 
 int main() {
